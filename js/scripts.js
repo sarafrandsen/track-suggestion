5ea8d3184e784d89dfb171003e2577408f63e26a
Sara Frandsen <sara.frandsen@gmail.com>
2017-01-17 01:23:06 -0800

    finally make everything work after removing all js and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,8 @@
 
 $(document).ready(function () {
-//show/hide next page
-
-  $("#toQ1").click(function() {
-    $("#intro").hide();
-    $("#question1").show();
-  });
-  $("#toQ2").click(function() {
-    $("#question1").hide();
-    $("#question2").show();
-  });
-  $("#toQ3").click(function() {
-    $("#question2").hide();
-    $("#question3").show();
-  });
-  $("#toQ4").click(function() {
-    $("#question3").hide();
-    $("#question4").show();
-  });
-  $("#toQ5").click(function() {
-    $("#question4").hide();
-    $("#question5").show();
-    $(".button").show();
-  });
-  $("#toResults").click(function() {
-    $("#question5").hide();
+  $("form#suggestedTrack").submit(function(event) {
     $("#results").show();
-  });
-
-  $("form#track").submit(function(event) {
+    $("#suggestedTrack").hide();
 
     var q1 = parseInt($("input:radio[name=q1]:checked").val());
     var q2 = parseInt($("input:radio[name=q2]:checked").val());
@@ -37,7 +11,7 @@ $(document).ready(function () {
     var q5 = parseInt($("input:radio[name=q5]:checked").val());
     var track = q1 + q2 + q3 + q4 + q5 ;
     var name = $("input#name").val();
-      $(".name").text(name);
+      $(".yourName").text(name);
 
     if (track >= 5) {
       $(".cSharp").show();
