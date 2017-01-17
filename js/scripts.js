f521b30137e919af23efe951fff7493aaec02a2c
Sara Frandsen <sara.frandsen@gmail.com>
2017-01-17 00:02:00 -0800

    add more to js

--- js/scripts.js
+++ js/scripts.js
@@ -30,12 +30,30 @@ $(document).ready(function () {
     $("#results").toggle();
   });
 
-
-  $("form").submit(function(event) {
+  $("form#suggestedTrack").submit(function(event) {
     event.preventDefault();
-    var name = ($("input#name").val());
-    $("#results").text(result);
-    $("")
-  });
 
+    var value1 = parseInt($("input:radio[name=q1]:checked").val());
+    var value2 = parseInt($("input:radio[name=q2]:checked").val());
+    var value3 = parseInt($("input:radio[name=q3]:checked").val());
+    var value4 = parseInt($("input:radio[name=q4]:checked").val());
+    var value5 = parseInt($("input:radio[name=q5]:checked").val());
+    var track = q1 + q2 + q3 + q4 + q5 ;
+    var name = $("input#name").val();
+
+    $("#results").text(name);
+
+    if (track >= 5) {
+      $("#cSharp").show();
+
+    } else if (track >= 6 && track <= 10) {
+      $("#java").show();
+
+    } else if (track >= 11 && track <= 15) {
+      $("#php").show();
+
+    }else if (track >= 16) {
+      $("#ruby").show();
+    }
+  });
 });
