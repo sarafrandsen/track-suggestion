40b908af9267d3230eed471557f04ba372bc75f4
Sara Frandsen <sara.frandsen@gmail.com>
2017-01-17 05:10:23 -0800

    use alerts to prevent empty inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,50 @@
 
 $(document).ready(function () {
   $(".continue").click(function() {
-    $("#intro").hide();
-    $("#questions").show();
-  })
+    $("#intro").fadeOut(function() {
+      $("#questions").fadeIn();
+    });
+  });
 
   $("form#suggestedTrack").submit(function(event) {
-    $("#results").show();
-    $("#suggestedTrack").hide();
+    event.preventDefault();
 
     var q1 = parseInt($("input:radio[name=q1]:checked").val());
     var q2 = parseInt($("input:radio[name=q2]:checked").val());
     var q3 = parseInt($("input:radio[name=q3]:checked").val());
     var q4 = parseInt($("input:radio[name=q4]:checked").val());
     var q5 = parseInt($("input:radio[name=q5]:checked").val());
-    var track = q1 + q2 + q3 + q4 + q5 ;
+
+    if (isNaN(q1) || isNaN(q2) || isNaN(q3) || isNaN(q4) || isNaN(q5)) {
+      alert("Please answer all the questions!");
+      return;
+    }
+
+    var track = q1 + q2 + q3 + q4 + q5;
+
+    $("#results").fadeOut(function() {
+      $("#suggestedTrack").fadeIn();
+    });
+
     var name = $("input#name").val();
-      $(".yourName").text(name);
+    $(".yourName").text(name);
 
     if (track <= 5) {
       $(".cSharp").show();
-
     } else if (track >= 6 && track <= 10) {
       $(".java").show();
-
     } else if (track >= 11 && track <= 15) {
       $(".php").show();
-
-    }else if (track >= 16) {
+    } else if (track >= 16) {
       $(".ruby").show();
     }
 
     $("#refresh").click(function() {
       location.reload();
     });
-    
-    event.preventDefault();
+
+    $("#suggestedTrack").fadeOut(function() {
+      $("#results").fadeIn();
+    });
   });
 });
